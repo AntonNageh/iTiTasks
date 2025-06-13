c1e7f5af76a96e772963df951bf5469f4d88a23d
Anton Nageh <anton.nageh20@gmail.com>
2025-06-13 22:43:08 +0300

    FixedFeatured and Special pages

--- Nextjs/Day1/app/news/feature/page.jsx
+++ Nextjs/Day1/app/news/feature/page.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-export const revalidate = 10;
-export default async function page() {
-  const res = await fetch('data/data.json');
-  const news = await res.json();
-
+'use client';
+import React, { useEffect, useState } from 'react'
+export default function page() {
+  const [data, setData] = useState(null);
+  useEffect(()=>{
+    fetch('/data/data.json')
+    .then(res => res.json())
+    .then(data => setData(data))
+  },[])
+  const news = data;
   return (
     <div>
       <h1>Featured News (ISR)</h1>
       {/* <p>Updated at {now}</p> */}
       <div className='mt-5'>
-        {news.articles.map((article) => (
+        {news && !news.articles && <p>Loading...</p>}
+        {news?.articles.map((article) => (
           <div key={article.id} className='py-5'>
             <h2>{article.title}</h2>
             <p>{article.description}</p>
--- Nextjs/Day1/app/news/special/page.jsx
+++ Nextjs/Day1/app/news/special/page.jsx
@@ -1,19 +1,28 @@
+'use client';
+import React, { useEffect, useState } from 'react'
 
-import React from 'react';
-
-export default  function SpecialNewsPage() {
-  const response =  fetch('data/data.json');
-  const newsData =  response.json();
+export default function Page() {
+  const [data, setData] = useState(null);
+  useEffect(()=>{
+    fetch('/data/data.json')
+    .then(res => res.json())
+    .then(data => setData(data))
+  },[])
+  const newsData = data;
   return (
     <div>
       <h1>Special News (SSR)</h1>
       <div className='mt-5'>
-        {newsData.articles.map((article) => (
-          <div key={article.id} className='py-5'>
-            <h2>{article.title}</h2>
-            <p>{article.description}</p>
-          </div>
-        ))}
+        {newsData && newsData.articles ? (
+          newsData.articles.map((article) => (
+            <div key={article.id} className='py-5'>
+              <h2>{article.title}</h2>
+              <p>{article.description}</p>
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </div>
   );
